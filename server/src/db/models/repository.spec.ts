56af3bf208d723bd133180e71f7465617db65c8e
nsathiya <narensathiya92@gmail.com>
2020-04-12 23:55:43 -0700

    test: refactor repository model tests

--- server/src/db/models/repository.spec.ts
+++ server/src/db/models/repository.spec.ts
@@ -4,55 +4,57 @@ import { getAllFriendsForUser } from './repository';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Repository - #getAllFriendsForUser', () => {
+describe('Repository -', () => {
   beforeEach(async () => {
     await Friendship.truncate({ cascade: true });
     await User.truncate({ cascade: true });
   });
 
-  it('get all friends for user (w/ friendships saved as user)', async () => {
-    const userA = await User.createUser({ userId: 'userA' });
-    const userB = await User.createUser({ userId: 'userB' });
-    const userC = await User.createUser({ userId: 'userC' });
+  describe('#getAllFriendsForUser', () => {
+    it('get all friends for user (w/ friendships saved as user)', async () => {
+      const userA = await User.createUser({ userId: 'userA' });
+      const userB = await User.createUser({ userId: 'userB' });
+      const userC = await User.createUser({ userId: 'userC' });
 
-    await Friendship.createFriendship({
-      user: userA.id,
-      friend: userB.id,
-      status: FriendStatus.PENDING
+      await Friendship.createFriendship({
+        user: userA.id,
+        friend: userB.id,
+        status: FriendStatus.PENDING
+      });
+
+      await Friendship.createFriendship({
+        user: userA.id,
+        friend: userC.id,
+        status: FriendStatus.ACTIVE
+      });
+
+      const friends = await getAllFriendsForUser(userA.id);
+      expect(friends[0].target.user_id).to.equal(userC.user_id);
+      expect(friends[1].target.user_id).to.equal(userB.user_id);
     });
 
-    await Friendship.createFriendship({
-      user: userA.id,
-      friend: userC.id,
-      status: FriendStatus.ACTIVE
+    it('get all friends for user (w/ friendships saved as friend)', async () => {
+      const userA = await User.createUser({ userId: 'userA' });
+      const userB = await User.createUser({ userId: 'userB' });
+      const userC = await User.createUser({ userId: 'userC' });
+
+      await Friendship.createFriendship({
+        user: userA.id,
+        friend: userB.id,
+        status: FriendStatus.PENDING
+      });
+
+      await Friendship.createFriendship({
+        user: userA.id,
+        friend: userC.id,
+        status: FriendStatus.ACTIVE
+      });
+
+      const friends = await getAllFriendsForUser(userC.id);
+
+      expect(friends[0].seeker.user_id).to.equal(userA.user_id);
+      expect(friends[0].target.user_id).to.equal(userC.user_id);
     });
-
-    const friends = await getAllFriendsForUser(userA.id);
-    expect(friends[0].target.user_id).to.equal(userC.user_id);
-    expect(friends[1].target.user_id).to.equal(userB.user_id);
-  });
-
-  it('get all friends for user (w/ friendships saved as friend)', async () => {
-    const userA = await User.createUser({ userId: 'userA' });
-    const userB = await User.createUser({ userId: 'userB' });
-    const userC = await User.createUser({ userId: 'userC' });
-
-    await Friendship.createFriendship({
-      user: userA.id,
-      friend: userB.id,
-      status: FriendStatus.PENDING
-    });
-
-    await Friendship.createFriendship({
-      user: userA.id,
-      friend: userC.id,
-      status: FriendStatus.ACTIVE
-    });
-
-    const friends = await getAllFriendsForUser(userC.id);
-
-    expect(friends[0].seeker.user_id).to.equal(userA.user_id);
-    expect(friends[0].target.user_id).to.equal(userC.user_id);
   });
 
 });
